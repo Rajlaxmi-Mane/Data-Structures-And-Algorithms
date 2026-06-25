0c5bec0ca64cce56ccfbddde128668d1ddf63ec4
Rajlaxmi-Mane <maithilimanepatil0424@gmail.com>
2026-06-25 10:31:08 +0530

    Add files via upload
    one more option added

--- patterns/AlphaNumPiramid.java
+++ patterns/AlphaNumPiramid.java
@@ -10,25 +10,48 @@ package com.patterns;
 public class AlphaNumPiramid {
 	
 	public static void main(String[] args) {
-		int n=4;
-		int cnt=1;
-		char ch='A';
+//     ----------------------------- OPTION 1 ----------------------------------
 		
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<n+i;j++) {
-				if(i+j>n) {
-					if(i%2==0)
-						System.out.print(ch++ +" ");
-					else
-						System.out.print(cnt++ +" ");
-				}
-
-				else
+//		int n=4;
+//		int cnt=1;
+//		char ch='A';
+//		
+//		for(int i=1;i<=n;i++) {
+//			for(int j=1;j<n+i;j++) {
+//				if(i+j>n) {
+//					if(i%2==0)
+//						System.out.print(ch++ +" ");
+//					else
+//						System.out.print(cnt++ +" ");
+//				}
+//
+//				else
+//					System.out.print("  ");
+//				
+//			}
+//			System.out.println();
+//		}
+		
+//	     ----------------------------- OPTION 2 ----------------------------------
+		
+		int n=4;
+		int num=1;
+		char ch = 'A';
+		
+		for(int i=1; i<=n; i++) { 
+			System.out.print("   ");
+			for(int j=1; j<=n*2-1; j++) {
+				if(i+j > n && j-i < n) { 
+					if(i%2 == 0) {
+						System.out.print(ch++ + " ");
+					} else {
+						System.out.print(num++ + " ");
+					}
+				} else {
 					System.out.print("  ");
-				
+				}
 			}
 			System.out.println();
 		}
 	}
-
 }
